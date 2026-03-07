87014295169fca88052b406ca361e7e56110e31a
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-07 12:32:28 +0800

    aray ko

--- db.js
+++ db.js
@@ -3,15 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Option A: Kung gusto mong gamitin ang mahabang URL (recommeded for Railway)
-// Gumawa ka ng variable sa Render na ang pangalan ay DATABASE_URL 
-// at ang value ay yung buong mysql:// string mo.
-const connectionString = process.env.DATABASE_URL || process.env.DB_HOST;
+// Babasahin nito ang DATABASE_URL na nilagay mo sa Render
+const connectionString = process.env.DATABASE_URL;
 
 export const db = mysql.createPool({
-  uri: connectionString, // Gagamit ng URI para hindi na malito sa host
+  uri: connectionString, 
   ssl: {
-    rejectUnauthorized: false // Mahalaga ito para sa Render-to-Railway connection
+    rejectUnauthorized: false // Mahalaga ito para sa Railway connection
   },
   waitForConnections: true,
   connectionLimit: 10,
