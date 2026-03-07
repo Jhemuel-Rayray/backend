0eef3720d3f8f66d1555c6127da0227305675243
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-07 12:35:13 +0800

    aray ko

--- db.js
+++ db.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Babasahin nito ang DATABASE_URL na nilagay mo sa Render
+// Babasahin nito ang DATABASE_URL na nilagay mo sa Render Environment Variables
 const connectionString = process.env.DATABASE_URL;
 
 export const db = mysql.createPool({
+  // GAMITIN ANG uri PROPERTY PARA SA FULL CONNECTION STRING
   uri: connectionString, 
   ssl: {
-    rejectUnauthorized: false // Mahalaga ito para sa Railway connection
+    rejectUnauthorized: false // Mahalaga ito para sa koneksyon sa Railway
   },
   waitForConnections: true,
   connectionLimit: 10,
