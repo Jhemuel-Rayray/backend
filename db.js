982bc562deb6cb2b376cfe3be711f0e7e92afdb4
Jhemuel Rayray <jhemuel.rayray1@gmail.com>
2026-03-11 10:50:21 +0800

    fix railway database connection

--- db.js
+++ db.js
@@ -3,16 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Babasahin nito ang DATABASE_URL na nilagay mo sa Render Environment Variables
-const connectionString = process.env.DATABASE_URL;
-
-export const db = mysql.createPool({
-  // GAMITIN ANG uri PROPERTY PARA SA FULL CONNECTION STRING
-  uri: connectionString, 
-  ssl: {
-    rejectUnauthorized: false // Mahalaga ito para sa koneksyon sa Railway
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+export const db = mysql.createPool(process.env.DATABASE_URL);
